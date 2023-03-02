72f0d7654b8208a0c8e30779631b000c917dc477
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-02 21:27:15 +0700

    Lesson 0102 submission

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,6 @@
 SELECT 
-  order_line_id AS sales_order_line_key,
-  quantity,
-  unit_price
+  order_line_id AS sales_order_line_key
+  , quantity
+  , unit_price
+  , quantity*unit_price AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
