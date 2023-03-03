f06b746a5bd3797cd9bc5cbde42f9d07fdc7f9af
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-04 06:13:45 +0700

    End of Lesson 0104

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,5 @@
 SELECT 
-  stock_item_id AS product_key
-  , stock_item_name AS product_name
-  , brand AS brand_name
+  CAST(stock_item_id AS INT) AS product_key
+  , CAST(stock_item_name AS string)AS product_name
+  , CAST(brand AS string) AS brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,9 +1,10 @@
 SELECT 
-  order_line_id AS sales_order_line_key
-  , quantity
-  , unit_price
-  , tax_rate
-  , quantity*unit_price AS gross_amount
-  , quantity * unit_price * tax_rate / 100 AS tax_amount
-  , (quantity*unit_price) - (quantity * unit_price * tax_rate / 100) AS net_amount
+  CAST(order_line_id AS INT) AS sales_order_line_key
+  , CAST(stock_item_id AS INT) AS product_key
+  , CAST(quantity AS INT) AS quantity
+  , CAST(unit_price AS NUMERIC) AS unit_price
+  , CAST(tax_rate AS NUMERIC) AS tax_rate
+  , CAST(quantity AS INT) * CAST(unit_price AS NUMERIC) AS gross_amount
+  , CAST(unit_price AS NUMERIC) * CAST(unit_price AS NUMERIC) * CAST(tax_rate AS NUMERIC) / 100 AS tax_amount
+  , (CAST(quantity AS INT) * CAST(unit_price AS NUMERIC)) - (CAST(quantity AS INT) * CAST(unit_price AS NUMERIC) * CAST(tax_rate AS NUMERIC) / 100) AS net_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
