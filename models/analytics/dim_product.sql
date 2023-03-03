592bd89e03d348c929154e6e5a53ceac22959fa6
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-04 05:13:30 +0700

    Add tax_rate & calculated measre [0103a] Lesson Completed

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,3 +1,5 @@
 SELECT 
-  *
+  stock_item_id AS product_key
+  , stock_item_name AS product_name
+  , brand AS brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,5 +2,8 @@ SELECT
   order_line_id AS sales_order_line_key
   , quantity
   , unit_price
+  , tax_rate
   , quantity*unit_price AS gross_amount
+  , quantity * unit_price * tax_rate / 100 AS tax_amount
+  , (quantity*unit_price) - (quantity * unit_price * tax_rate / 100) AS net_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
