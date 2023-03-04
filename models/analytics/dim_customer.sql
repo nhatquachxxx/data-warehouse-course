a5cc1e39c8da2e987b02263229d9b2b103731f8c
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-04 16:17:51 +0700

    Lesson 0106a

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,17 @@
+WITH dim_customer__source AS (
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_cast_type AS (
+  SELECT
+    CAST(customer_id AS INT) AS customer_key
+    , CAST(customer_name AS STRING) AS customer_name
+  FROM dim_customer__source
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+  customer_key,
+  customer_name
+FROM dim_customer__rename_cast_type
