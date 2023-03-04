698e06c698925e700c6fd6ec3f7f6e3552520343
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-04 10:57:37 +0700

    Create CTE for layering code

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,18 @@
+-- 1st layer, select all resource from dim_prodct
+WITH dim_product__source AS(
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+-- 2nd layer, select specific fields and perform CAST
+, dim_product__rename_cast AS (
+  SELECT 
+      CAST(stock_item_id AS INT) AS product_key
+    , CAST(stock_item_name AS string)AS product_name
+    , CAST(brand AS string) AS brand_name
+  FROM dim_product__source
+)
+
 SELECT 
-  CAST(stock_item_id AS INT) AS product_key
-  , CAST(stock_item_name AS string)AS product_name
-  , CAST(brand AS string) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  * 
+FROM dim_product__rename_cast
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,10 +1,37 @@
+-- Code Layering with CTE
+-- Layer 1: select all from source table
+-- Layer 2: Select necessary fields & apply cast
+-- Layer 3: perform calculated measures
+-- Code Layering with CTE
+
+WITH fact_sales_order_line__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line__rename_cast AS (
+  SELECT 
+    CAST(order_line_id AS INT) AS sales_order_line_key
+    , CAST(stock_item_id AS INT) AS product_key
+    , CAST(quantity AS INT) AS quantity
+    , CAST(unit_price AS NUMERIC) AS unit_price
+    , CAST(tax_rate AS NUMERIC) AS tax_rate
+  FROM fact_sales_order_line__source
+)
+
+, fact_sales_order_line__calculated_measure AS (
+  SELECT 
+    sales_order_line_key
+    , product_key
+    , quantity
+    , unit_price
+    , tax_rate
+    , quantity * unit_price AS gross_amount
+    , unit_price * quantity *tax_rate AS tax_amount
+    , (quantity * unit_price) - (unit_price * quantity * tax_rate) AS net_amount -- net_amount = gross_amount - tax_amount
+  FROM fact_sales_order_line__rename_cast
+)
+
 SELECT 
-  CAST(order_line_id AS INT) AS sales_order_line_key
-  , CAST(stock_item_id AS INT) AS product_key
-  , CAST(quantity AS INT) AS quantity
-  , CAST(unit_price AS NUMERIC) AS unit_price
-  , CAST(tax_rate AS NUMERIC) AS tax_rate
-  , CAST(quantity AS INT) * CAST(unit_price AS NUMERIC) AS gross_amount
-  , CAST(unit_price AS NUMERIC) * CAST(unit_price AS NUMERIC) * CAST(tax_rate AS NUMERIC) / 100 AS tax_amount
-  , (CAST(quantity AS INT) * CAST(unit_price AS NUMERIC)) - (CAST(quantity AS INT) * CAST(unit_price AS NUMERIC) * CAST(tax_rate AS NUMERIC) / 100) AS net_amount
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  *
+FROM fact_sales_order_line__calculated_measure
