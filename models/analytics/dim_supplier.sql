f33cab58fca0291b0b23017fe06fe288901361dd
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-08 05:31:27 +0700

    Declare table dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,16 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+WITH dim_supplier__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_recast AS (
+  SELECT
+    CAST(supplier_id AS INT) AS supplier_key
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM dim_supplier__source
+)
+
+SELECT
+  supplier_key
+  , supplier_name
+FROM dim_supplier__rename_recast
