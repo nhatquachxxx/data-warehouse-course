e1b163f1f3733a98cf9c392df60196f6ce778ea6
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-08 05:49:58 +0700

    Flatten supplier key/name into dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,4 +1,4 @@
--- 1st layer, select all resource from dim_prodct
+-- 1st layer, select all resource from dim_product
 WITH dim_product__source AS(
   SELECT *
   FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
@@ -8,11 +8,18 @@ WITH dim_product__source AS(
 , dim_product__rename_recast AS (
   SELECT 
       CAST(stock_item_id AS INT) AS product_key
-    , CAST(stock_item_name AS string)AS product_name
+    , CAST(stock_item_name AS string) AS product_name
+    , CAST(supplier_id AS INT) AS supplier_key
     , CAST(brand AS string) AS brand_name
   FROM dim_product__source
 )
 
-SELECT 
-  * 
-FROM dim_product__rename_recast
+SELECT
+  dim_product.product_key
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_product.supplier_key
+  , dim_supplier.supplier_name
+FROM dim_product__rename_recast AS dim_product
+JOIN {{ ref('dim_supplier') }} AS dim_supplier
+  ON dim_product.supplier_key = dim_supplier.supplier_key
