ac3ace7cfb432106022665cacf3899143a6c094c
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-04-01 16:23:31 +0700

    declare role-palying dimensions

--- models/analytics/role_playing_dimensions/dim_person_picked_by.sql
+++ models/analytics/role_playing_dimensions/dim_person_picked_by.sql
@@ -2,4 +2,8 @@ SELECT
   person_key AS picked_by_person_key
   , full_name AS picked_by_full_name
   , preferred_name AS picked_by_preferred_name
+  , is_employee
+  , is_salesperson
 FROM {{ ref('dim_person') }}
+WHERE is_employee = 'Employee'
+  OR person_key IN (0, -1)
--- models/analytics/role_playing_dimensions/dim_person_salesperson.sql
+++ models/analytics/role_playing_dimensions/dim_person_salesperson.sql
@@ -3,3 +3,5 @@ SELECT
   , full_name AS salesperson_full_name
   , preferred_name AS salesperson_preferred_name
 FROM {{ ref('dim_person') }}
+WHERE is_salesperson = 'Salesperson'
+  OR person_key IN (0, -1)
