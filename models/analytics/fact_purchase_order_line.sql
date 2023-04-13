ffbf26d3d5bbf84330e61c3ca40c78f652e3cda8
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-04-13 20:31:47 +0700

    Create Purchase Order module

---
+++ models/analytics/fact_purchase_order_line.sql
@@ -0,0 +1,78 @@
+WITH fact_purchase_order_line__source AS (
+  SELECT
+    *
+  FROM `vit-lam-data.wide_world_importers.purchasing__purchase_order_lines`
+)
+
+, fact_purchase_order_line__rename_recast AS (
+  SELECT
+    CAST(purchase_order_line_id AS INT) AS purchase_order_line_key
+    , CAST(purchase_order_id AS INT) AS purchase_order_key
+    , CAST(is_order_line_finalized AS BOOLEAN) AS is_order_line_finalized_boolean
+    , CAST(stock_item_id AS INT) AS product_key
+    , CAST(package_type_id AS INT) AS package_type_key
+    , CAST(ordered_outers AS INT) AS ordered_outers
+    , CAST(received_outers AS INT) AS received_outers
+    , CAST(expected_unit_price_per_outer AS NUMERIC) AS expected_unit_price_per_outer
+    , CAST(description AS string) AS description
+    , CAST(last_receipt_date AS DATE) AS last_receipt_date
+  FROM fact_purchase_order_line__source
+)
+
+, fact_purchase_order_line__convert_boolean AS (
+  SELECT
+    *
+    , CASE
+        WHEN is_order_line_finalized_boolean IS TRUE THEN 'Order Line Finalized'
+        WHEN is_order_line_finalized_boolean IS FALSE THEN 'Order Line Not Finalized '
+        WHEN is_order_line_finalized_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid' END
+    AS is_order_line_finalized
+  FROM fact_purchase_order_line__rename_recast
+)
+
+, fact_purchase_order_line__handle_null AS (
+  SELECT
+    purchase_order_line_key
+    , IFNULL(purchase_order_key, 0) AS purchase_order_key
+    , is_order_line_finalized
+    , IFNULL(product_key, 0) AS product_key
+    , IFNULL(package_type_key, 0) AS package_type_key
+    , ordered_outers
+    , received_outers
+    , expected_unit_price_per_outer
+    , IFNULL(description, 'Undefined') AS description
+    , last_receipt_date
+  FROM fact_purchase_order_line__convert_boolean
+)
+
+, fact_purchase_order_line__calculate_measure AS (
+  SELECT
+    *
+    , ordered_outers - received_outers AS remaining_outers
+  FROM fact_purchase_order_line__handle_null
+)
+
+SELECT
+  fact_line.purchase_order_line_key
+  , fact_line.purchase_order_key
+  , fact_header.is_order_finalized
+  , fact_line.is_order_line_finalized
+  , fact_line.product_key
+  , fact_line.package_type_key
+  , fact_header.supplier_key
+  , fact_header.delivery_method_key
+  , fact_header.contact_person_key
+  , fact_line.description
+  , fact_header.supplier_reference
+  , fact_header.comments
+  , fact_line.ordered_outers
+  , fact_line.received_outers
+  , fact_line.remaining_outers
+  , fact_line.expected_unit_price_per_outer
+  , fact_line.last_receipt_date
+  , fact_header.order_date
+  , fact_header.expected_delivery_date
+FROM fact_purchase_order_line__calculate_measure AS fact_line
+LEFT JOIN {{ ref('stg_fact_purchase_order') }} AS fact_header
+  ON fact_line.purchase_order_key = fact_header.purchase_order_key
---
+++ models/staging/stg_fact_purchase_order.sql
@@ -0,0 +1,95 @@
+WITH fact_purchase_order__source AS (
+  SELECT
+    *
+  FROM `vit-lam-data.wide_world_importers.purchasing__purchase_orders`
+)
+
+, fact_purchase_order__rename_recast AS (
+  SELECT
+    CAST(purchase_order_id AS INT) AS purchase_order_key
+    , CAST(is_order_finalized AS BOOLEAN) AS is_order_finalized_boolean
+    , CAST(supplier_id AS INT) AS supplier_key
+    , CAST(delivery_method_id AS INT) AS delivery_method_key
+    , CAST(contact_person_id AS INT) AS contact_person_key
+    , CAST(supplier_reference AS STRING) AS supplier_reference
+    , CAST(comments AS STRING) AS comments
+    , CAST(order_date AS DATE) AS order_date
+    , CAST(expected_delivery_date AS DATE) AS expected_delivery_date
+  FROM fact_purchase_order__source
+)
+
+, fact_purchase_order__convert_boolean AS (
+  SELECT
+    *
+    , CASE
+        WHEN is_order_finalized_boolean IS TRUE THEN 'Order Finalized'
+        WHEN is_order_finalized_boolean IS FALSE THEN 'Order Not Finalized'
+        WHEN is_order_finalized_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid' END
+      AS is_order_finalized
+  FROM fact_purchase_order__rename_recast
+)
+
+, fact_purchase_order__handle_null AS (
+  SELECT
+    purchase_order_key
+    , is_order_finalized
+    , IFNULL(supplier_key, 0) AS supplier_key
+    , IFNULL(delivery_method_key, 0) AS delivery_method_key
+    , IFNULL(contact_person_key, 0) AS contact_person_key
+    , IFNULL(supplier_reference, 'Undefined') AS supplier_reference
+    , IFNULL(comments, 'Undefined') AS comments
+    , order_date
+    , expected_delivery_date
+  FROM fact_purchase_order__convert_boolean
+)
+
+, fact_purchase_order__add_undefined AS (
+  SELECT
+    purchase_order_key
+    , is_order_finalized
+    , supplier_key
+    , delivery_method_key
+    , contact_person_key
+    , supplier_reference
+    , comments
+    , order_date
+    , expected_delivery_date
+  FROM fact_purchase_order__handle_null
+
+  UNION ALL
+  SELECT
+    0 AS purchase_order_key
+    , 'Undefined' AS is_order_finalized
+    , 0 AS supplier_key
+    , 0 AS delivery_method_key
+    , 0 AS contact_person_key
+    , 'Undefined' AS supplier_reference
+    , 'Undefined' AS comments
+    , NULL AS order_date
+    , NULL AS expected_delivery_date
+
+  UNION ALL
+  SELECT
+    -1 AS purchase_order_key
+    , 'Invalid' AS is_order_finalized
+    , -1 AS supplier_key
+    , -1 AS delivery_method_key
+    , -1 AS contact_person_key
+    , 'Invalid' AS supplier_reference
+    , 'Invalid' AS comments
+    , NULL AS order_date
+    , NULL AS expected_delivery_date
+)
+
+SELECT
+  purchase_order_key
+  , is_order_finalized
+  , supplier_key
+  , delivery_method_key
+  , contact_person_key
+  , supplier_reference
+  , comments
+  , order_date
+  , expected_delivery_date
+FROM fact_purchase_order__add_undefined
